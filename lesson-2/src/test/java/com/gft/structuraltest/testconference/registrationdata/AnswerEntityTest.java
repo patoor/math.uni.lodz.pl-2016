50d40c7a4aa8b0166987d5643cfb9eca75c8a781
pator <pator0509@wp.pl>
2016-11-17 22:17:29 +0100

    Homework

--- lesson-2/src/test/java/com/gft/structuraltest/testconference/registrationdata/AnswerEntityTest.java
+++ lesson-2/src/test/java/com/gft/structuraltest/testconference/registrationdata/AnswerEntityTest.java
@@ -11,5 +11,86 @@ public class AnswerEntityTest{
         AnswerEntity an2 = null;
         assertFalse(an1.equals(an2));
     }
-    
+    @Test
+	public void shouldReturnTrueWhenTheSameObj()
+	{
+		AnswerEntity a1 = new AnswerEntity();
+		AnswerEntity a2 = a1;
+		assertTrue(a1.equals(a2));
+	}
+	@Test 
+	public void shouldRetutnFalseWhenDiffClass()
+	{
+		AnswerEntity a1 = new AnswerEntity();
+		QuestionEntity q1 =new QuestionEntity();
+		assertFalse(a1.equals(q1));
+	}
+	@Test
+	public void shhoudReturnFalseWhenAnserTextisNull()
+	{
+		AnswerEntity a1 = new AnswerEntity();
+		AnswerEntity a2 = new AnswerEntity();
+		a2.setAnswerText("aaa");
+		assertFalse(a1.equals(a2));
+	}
+	
+	@Test
+	public void shouldReturnFlaseWhenTextiISDifferent()
+	{
+		AnswerEntity a1 = new AnswerEntity();
+		AnswerEntity a2 = new AnswerEntity();
+		a1.setAnswerText("aaa");
+		a2.setAnswerText("bbb");
+		assertFalse(a1.equals(a2));
+	}
+	@Test
+	public void shouldReturnfalsewhenOtherIdIsNN()
+	{
+		AnswerEntity a1 = new AnswerEntity();
+		AnswerEntity a2 = new AnswerEntity();
+		a1.setAnswerText("aaa");
+		a2.setAnswerText("bbb");
+		a2.setId(new Long(3));
+		assertFalse(a1.equals(a2));
+	}
+	
+	@Test 
+	public void shouldReturnFalseWhenIdIsDiffernt()
+	{
+		AnswerEntity a1 = new AnswerEntity();
+		AnswerEntity a2 = new AnswerEntity();
+		a1.setAnswerText("aaa");
+		a2.setAnswerText("bbb");
+		a1.setId(new Long(1));
+		a2.setId(new Long(3));
+		assertFalse(a1.equals(a2));			
+	}
+	
+	@Test 
+	public void shouldReturnFalseWhenQuestionIsNull()
+	{
+		AnswerEntity a1 = new AnswerEntity();
+		AnswerEntity a2 = new AnswerEntity();
+		a1.setAnswerText("aaa");
+		a2.setAnswerText("bbb");
+		a1.setId(new Long(3));
+		a2.setId(new Long(3));
+		a2.setQuestion(new QuestionEntity());
+		assertFalse(a1.equals(a2));		
+	}
+	
+	@Test
+	public void shouldReturnFalseWhenQuestionIsDifferent()
+	{
+		AnswerEntity a1 = new AnswerEntity();
+		AnswerEntity a2 = new AnswerEntity();
+		a1.setAnswerText("aaa");
+		a2.setAnswerText("bbb");
+		a1.setId(new Long(3));
+		a2.setId(new Long(3));
+		a1.setQuestion(new QuestionEntity());
+		a2.setQuestion(new QuestionEntity());
+		assertFalse(a1.equals(a2));
+	}
 }
+	
